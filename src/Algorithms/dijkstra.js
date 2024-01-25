5e0b1b86b1368f09b0312d3dfda885819b6b5671
TheCowPlays <orvol.hauptman@gmail.com>
2024-01-25 09:04:38 +0100

    added A* algorithm

--- src/Algorithms/dijkstra.js
+++ src/Algorithms/dijkstra.js
@@ -24,14 +24,14 @@ function sortNodesByDistance(unvisitedNodes){
 }
 
 function updateUnvisitedNeighbors(node, grid){
-    const unvisitedNeighbors = getUnvisidedNeighbors(node, grid);
+    const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
     for(const neighbor of unvisitedNeighbors){
         neighbor.distance = node.distance + 1;
         neighbor.previousNode = node;
     }
 }
 
-function getUnvisidedNeighbors(node, grid){
+function getUnvisitedNeighbors(node, grid){
     const neighbors = [];
     const {col, row} = node;
     if(row > 0) neighbors.push(grid[row - 1][col]);
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -3,14 +3,17 @@ import Node from './Node/Node';
 import './PathfindingVisualizer.css'
 // Algorithms
 import { dijkstra, getNodesInShortestPathOrder } from '../Algorithms/dijkstra';
+import { astar } from '../Algorithms/astar';
 
-const START_NODE_ROW = 10;
-const START_NODE_COL = 15;
-const END_NODE_ROW = 10;
-const END_NODE_COL = 35;
+
+const START_NODE_ROW = 0;
+const START_NODE_COL = 0;
+const END_NODE_ROW = 19;
+const END_NODE_COL = 19;
 
 
 export default class PathfindingVisualizer extends Component {
+
     constructor() {
         super();
         this.state = {
@@ -74,12 +77,45 @@ export default class PathfindingVisualizer extends Component {
         this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
     }
 
+    // A* algorithm
+    animateAStar(visitedNodesInOrder, nodesInShortestPathOrder) {
+        for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+            if (i === nodesInShortestPathOrder.length) {
+                setTimeout(() => {
+                    this.animateShortestPath(nodesInShortestPathOrder);
+                }, 10 * i);
+                return;
+            }
+            setTimeout(() => {
+                const node = visitedNodesInOrder[i];
+                document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-visited';
+            }, 10 * i);
+        }
+    }
+
+    visualizeAStar() {
+        const { grid } = this.state;
+        const startNode = grid[START_NODE_ROW][START_NODE_COL];
+        const endNode = grid[END_NODE_ROW][END_NODE_COL];
+
+        const visitedNodesInOrder = astar(grid, startNode, endNode);
+        const nodesInShortestPathOrder = getNodesInShortestPathOrder(endNode);
+        this.animateAStar(visitedNodesInOrder, nodesInShortestPathOrder);
+    }
+
     render() {
         const { grid, mouseIsPressed } = this.state;
+        console.log(grid);
         return (
             <>
+                <button onClick={() => this.generateMaze(this.state.grid)}>
+                    Generate Maze
+                </button>
                 <button onClick={() => this.visualizeDijkstra()}>
                     Dijkstra's Algorithm
+                </button>
+                <button onClick={() => this.visualizeAStar()}>
+                    A* Algorithm
                 </button>
                 <div className="grid">
                     {grid.map((row, rowIndex) => {
@@ -107,6 +143,7 @@ export default class PathfindingVisualizer extends Component {
                 </div>
             </>
         );
+
     }
 }
 
@@ -114,7 +151,7 @@ const getInitialGrid = () => {
     const grid = [];
     for (let row = 0; row < 20; row++) {
         const currentRow = [];
-        for (let col = 0; col < 50; col++) {
+        for (let col = 0; col < 20; col++) {
             currentRow.push(createNode(col, row));
         }
         grid.push(currentRow);
